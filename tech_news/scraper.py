70997497f3f04b2c0513e0c5198430366be5e5a1
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2022-03-13 17:50:35 -0300

    resolve requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -51,7 +51,56 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("main h1.tec--article__header__title::text").get()
+    timestamp = selector.css("main time::attr(datetime)").get()
+
+    writer = (
+        selector.css("main div.tec--author__info *::text").get()
+        or selector.css("main div.tec--timestamp__item a::text").get()
+    ).strip() or None
+
+    shares_count = int(
+        selector.css("main div.tec--toolbar__item::text").re_first(r"\d+") or 0
+    )
+
+    comments_count = int(
+        selector.css("main button.tec--btn::text").re_first(r"\d+")
+    )
+
+    summary = selector.css(
+        "main div.tec--article__body p:first-child *::text"
+    ).getall()
+    summary = "".join(summary)
+
+    sources = selector.css("div.z--mb-16 h2 ~ div a.tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
+
+    categories = selector.css("main a.tec--badge--primary::text").getall()
+    categories = [category.strip() for category in categories]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+
+
+# print(
+#     scrape_noticia(
+#         fetch(
+#             "https://www.tecmundo.com.br/minha-serie/215330-8-series-parecidas-the-crown-fas-realeza.htm"
+#         )
+#     )
+# )
 
 
 # Requisito 5
